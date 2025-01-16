8e1a814d74a06b95f9c081f78929e2968f7075d6
eman-nawzad <eman.nawzad00@gmail.com>
2025-01-16 21:35:41 +0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ from streamlit_folium import st_folium
 import geopandas as gpd
 
 # Define dataset paths
-data_file = "data/sppi.geojson"  # Replace with the correct path to your GeoJSON file
+data_file = "data/spi_.geojson"  # Replace with the correct path to your GeoJSON file
 
 # Load the dataset
 gdf = gpd.read_file(data_file)
